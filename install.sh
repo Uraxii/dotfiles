8ef021e1d91abdc9397ba1757863a1a67dfcf0bd
uraxii <accounts@brandonpaul.me>
2024-10-19 21:34:27 -0400

    upaded install file

--- install.sh
+++ install.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
-cp -r .config ~/.config/
+config_dir=~/.config/
+
+cp -r ./hyper $config_dir
+#cp -r ./rofi $config_dir
+cp -r ./tofi $config_dir
+cp -r ./nvim $config_dir
+cp ./.zshrc $HOME/.zshrc
