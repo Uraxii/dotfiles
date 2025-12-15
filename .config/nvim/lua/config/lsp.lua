6c377a7e11f8a7d12ddcf7819f086087533764fb
Uraxii <accounts@nicolepaul.net>
2025-12-15 12:29:33 -0500

    Gdscript lsp support

--- .config/nvim/lua/config/lsp.lua
+++ .config/nvim/lua/config/lsp.lua
@@ -1,10 +1,15 @@
+-- LSP config files can be found on the nvim-lspconfig repository.
+-- https://github.com/neovim/nvim-lspconfig/tree/master/lsp
+
+-- Lua files located in root/lsp
 vim.lsp.enable {
+  'gdscript',
   'lua_ls',
 }
 
 vim.diagnostic.config {
   virtual_lines = true,
-  -- virtual_text = true,
+  virtual_text = true,
   underline = true,
   update_in_insert = false,
   severity_sort = true,
@@ -25,3 +30,23 @@ vim.diagnostic.config {
     },
   },
 }
+
+-- This checks if we are in a Godot project.
+-- If we are, then listen to the godothost file.
+-- This allows the Godot editor to do things like open files in nvim without needing to create a new instance of nvim.
+
+-- Ensure the Godot editor options are set:
+-- Use External Editor: On
+-- Normal setup:
+--  Exec Path: nvim
+--  Exec Flags: --server ./godothost.pipe --remote-send "<C-\><C-N>:drop {file}<CR>{line}G{col}|"
+-- If godot is sandboxed (i.e. running as a flatpak)
+--  Exec Path: /usr/bin/flatpak-spawn
+--  Exec Flags: --host /usr/bin/nvim --server ./godothost.pipe --remote-send "<C-\><C-N>:drop {file}<CR>{line}G{col}|"
+
+local gdproject = io.open(vim.fn.getcwd() .. '/project.godot', 'r')
+if gdproject then
+  io.close(gdproject)
+  vim.fn.serverstart './godothost.pipe'
+  vim.notify 'Starting Godot server.'
+end
